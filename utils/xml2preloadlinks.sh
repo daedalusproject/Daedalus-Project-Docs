ffbd528ce21f31a2a442a4d2aec91e34966e1d3d
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2019-11-23 13:03:05 +0100

    Test font preload

--- utils/xml2preloadlinks.sh
+++ utils/xml2preloadlinks.sh
@@ -8,48 +8,49 @@ VHOSTLOCATIONS=$TMPFOLDER/vhostlocations
 
 SITEMAP_XML=$1
 
-PUBLIC_PATH=$(echo $SITEMAP_XML | sed  's/\/[^\/]\+$//g')
+PUBLIC_PATH=$(echo $SITEMAP_XML | sed 's/\/[^\/]\+$//g')
 
 mkdir -p $TMPFOLDER
 
 rm -f $VHOSTLOCATIONS
-touch  $VHOSTLOCATIONS
+touch $VHOSTLOCATIONS
 
 if [ -f $SITEMAP_XML ]; then
-    xmllint --format $SITEMAP_XML -o $TMPFOLDER/sitemap.xml
-    locations=$(xmlstarlet sel -t -v '//*[local-name()="loc"]' /var/tmp/xml2preloadlinks/sitemap.xml  | grep -v "/tags/\|/categories/")
-    for location in $locations
-    do
-        INDEXTOPROCESS="$PUBLIC_PATH$location"index.html
-        if [ -f $INDEXTOPROCESS ]; then
-            rm -f $LINKS
-            for link in $(grep -o "src=[^>]*.js>" $INDEXTOPROCESS | sed 's/^src=\([^>]*\)>$/\1/')
-            do
-                echo "        add_header Link \"<$link>; rel=preload; as=script\";"; 
-            done > $LINKS
-            for link in $(grep -o "href=[^>]*.css" $INDEXTOPROCESS | sed 's/^href=\([^>]*\)$/\1/')
-            do
-                echo "        add_header Link \"<$link>; rel=preload; as=style\";"
-            done >> $LINKS
-            for link in $(grep -o "href=[^>]*type=image" $INDEXTOPROCESS | sed 's/^href=\([^ ]*\) type=image$/\1/')
-            do
-                echo "        add_header Link \"<$link>; rel=preload; as=image\";"
-            done >> $LINKS
-            for link in $(grep -o "img src=[^ ]*" $INDEXTOPROCESS | sed 's/^img src=\([^ ]*\)$/\1/')
-            do
-                echo "        add_header Link \"<$link>; rel=preload; as=image\";"
-            done >> $LINKS
+	xmllint --format $SITEMAP_XML -o $TMPFOLDER/sitemap.xml
+	locations=$(xmlstarlet sel -t -v '//*[local-name()="loc"]' /var/tmp/xml2preloadlinks/sitemap.xml | grep -v "/tags/\|/categories/")
+	for location in $locations; do
+		INDEXTOPROCESS="$PUBLIC_PATH$location"index.html
+		if [ -f $INDEXTOPROCESS ]; then
+			rm -f $LINKS
+			for link in $(grep -o "src=[^>]*.js>" $INDEXTOPROCESS | sed 's/^src=\([^>]*\)>$/\1/'); do
+				echo "        add_header Link \"<$link>; rel=preload; as=script\";"
+			done >$LINKS
+			for link in $(grep -o "href=[^>]*.css" $INDEXTOPROCESS | sed 's/^href=\([^>]*\)$/\1/'); do
+				echo "        add_header Link \"<$link>; rel=preload; as=style\";"
+			done >>$LINKS
+			for link in $(grep -o "href=[^>]*type=image" $INDEXTOPROCESS | sed 's/^href=\([^ ]*\) type=image$/\1/'); do
+				echo "        add_header Link \"<$link>; rel=preload; as=image\";"
+			done >>$LINKS
+			for link in $(grep -o "img src=[^ ]*" $INDEXTOPROCESS | sed 's/^img src=\([^ ]*\)$/\1/'); do
+				echo "        add_header Link \"<$link>; rel=preload; as=image\";"
+			done >>$LINKS
 
-            if [ -f $LINKS ]; then
-                echo "    location $location {" >> $VHOSTLOCATIONS
-                echo "        index index.html;" >> $VHOSTLOCATIONS
-                cat $LINKS >> $VHOSTLOCATIONS
-                echo -e "    }\n" >> $VHOSTLOCATIONS
-            fi
-        fi
-    done
-    cp $VHOSTLOCATIONS vhostlocations.conf
+			# Fonts
+			echo "        add_header Link \"</fonts/Novecentosanswide-UltraLight-webfont.woff2>; rel=preload; as=font\";" >>$LINKS
+			echo "        add_header Link \"</fonts/Lato-Regular.woff2>; rel=preload; as=font\";" >>$LINKS
+			echo "        add_header Link \"</fonts/fontawesome-webfont.woff2?v=4.7.0>; rel=preload; as=font\";" >>$LINKS
+			echo "        add_header Link \"</fonts/Lato-Bold.woff2>; rel=preload; as=font\";" >>$LINKS
+
+			if [ -f $LINKS ]; then
+				echo "    location $location {" >>$VHOSTLOCATIONS
+				echo "        index index.html;" >>$VHOSTLOCATIONS
+				cat $LINKS >>$VHOSTLOCATIONS
+				echo -e "    }\n" >>$VHOSTLOCATIONS
+			fi
+		fi
+	done
+	cp $VHOSTLOCATIONS vhostlocations.conf
 else
-    (>&2 echo "$SITEMAP_XML not found.")
-    exit 1
+	(echo >&2 "$SITEMAP_XML not found.")
+	exit 1
 fi
